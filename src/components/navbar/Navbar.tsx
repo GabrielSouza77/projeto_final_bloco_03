9469a8e4ac76425630e42f8b54288b3e66556dd5
Gabriel Souza <souzagabriels@hotmail.com>
2025-07-02 11:20:45 -0300

    :art: CRUD

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import {
 } from "@phosphor-icons/react";
 import { useRef } from "react";
 import logo from "../../assets/imgs/farmacia.png";
+import { Link } from "react-router-dom";
 
 type MenuState = "closed" | "open";
 
@@ -35,12 +36,12 @@ function Navbar({
     <>
       <div className="fixed md:relative top-0 left-0 z-50 w-full bg-green-500 text-white flex justify-center py-4 md:py-2">
         <div className="container mx-6 mt-2 md:mt-0 flex items-center justify-between text-lg">
-          <a href="/home " className="flex items-center">
+          <Link to="/home" className="flex items-center">
             <img src={logo} alt="Logo" className="w-20 md:w-24" />
             <span className="hidden md:inline text-3xl font-bold">
               Farmácia
             </span>
-          </a>
+          </Link>
           <div className="relative flex w-2/5 items-center justify-center text-black max-md:hidden">
             <form className="flex w-full items-center justify-center">
               <input
@@ -60,15 +61,12 @@ function Navbar({
             </form>
           </div>
           <div className="hidden md:flex items-center gap-4 py-4">
-            <a href="/produtos" className="hover:underline">
-              Produtos
-            </a>
-            <a href="/categorias" className="hover:underline">
+            <Link to="/categorias" className="hover:underline">
               Categorias
-            </a>
-            <a href="/cadcategoria" className="hover:underline">
+            </Link>
+            <Link to="/cadcategoria" className="hover:underline">
               Cadastrar Categoria
-            </a>
+            </Link>
             <UserIcon size={32} weight="bold" />
             <ShoppingCartIcon size={32} weight="bold" />
           </div>
@@ -86,16 +84,12 @@ function Navbar({
       {menuState === "open" && (
         <div
           ref={menuRef}
-          className="fixed top-0 left-0 z-50 h-full w-full bg-slate-800 bg-opacity-95 md:hidden transition-all duration-300 ease-in-out animate-fade-in animate-slide-in"
+          className="fixed top-0 left-0 z-50 h-full w-full bg-green-400 bg-opacity-95 md:hidden transition-all duration-300 ease-in-out animate-fade-in animate-slide-in"
           style={{ animation: "fade-in 0.3s, slide-in 0.3s" }}
         >
           <div className="relative flex flex-col items-start justify-start gap-2 p-6 text-left text-lg text-white">
             <div className="flex w-full items-center justify-between mb-2">
-              <img
-                src="https://ik.imagekit.io/vzr6ryejm/games/logolg.png?updatedAt=1705976699033"
-                alt="Logo"
-                className="w-50 md:w-60"
-              />
+              <img src={logo} alt="Logo" className="w-20 md:w-60" />
               <button
                 type="button"
                 aria-label="Fechar menu"
@@ -125,52 +119,38 @@ function Navbar({
                 />
               </button>
             </form>
-            <a
-              href="/home"
+            <Link
+              to="/home"
               onClick={handleMenuClose}
               className="py-2 text-white hover:text-gray-300"
             >
               Home
-            </a>
-            <a
-              href="/produtos"
-              onClick={handleMenuClose}
-              className="py-2 text-white hover:text-gray-300"
-            >
-              Produtos
-            </a>
-            <a
-              href="/cadproduto"
-              onClick={handleMenuClose}
-              className="py-2 text-white hover:text-gray-300"
-            >
-              Cadastrar Produto
-            </a>
-            <a
-              href="/categorias"
+            </Link>
+            <Link
+              to="/categorias"
               onClick={handleMenuClose}
               className="py-2 text-white hover:text-gray-300"
             >
               Categorias
-            </a>
-            <a
-              href="/cadcategoria"
+            </Link>
+            <Link
+              to="/cadcategoria"
               onClick={handleMenuClose}
               className="py-2 text-white hover:text-gray-300"
             >
               Cadastrar Categoria
-            </a>
+            </Link>
             <div className="mt-4 flex gap-4">
-              <a href="#" onClick={handleMenuClose}>
+              <Link to="#" onClick={handleMenuClose}>
                 <UserIcon size={32} weight="bold" className="text-white" />
-              </a>
-              <a href="#" onClick={handleMenuClose}>
+              </Link>
+              <Link to="#" onClick={handleMenuClose}>
                 <ShoppingCartIcon
                   size={32}
                   weight="bold"
                   className="text-white"
                 />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import homeImage from "../../assets/imgs/home.png";
 function Home() {
   return (
     <>
-      <div className="bg-[#ffffff] min-h-screen flex justify-center">
+      <div className="bg-[#ffffff] flex justify-center">
         <div className="container grid grid-cols-2 text-green-500">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
@@ -17,11 +17,7 @@ function Home() {
           </div>
 
           <div className="flex justify-center ">
-            <img
-              src={homeImage}
-              alt="Imagem Página Home"
-              className="w-2/3 py-80"
-            />
+            <img src={homeImage} alt="Imagem Página Home" className="w-2/3" />
           </div>
         </div>
       </div>
